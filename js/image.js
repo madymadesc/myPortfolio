173ed1e5ad7dd621a73d8b4851e5b6d08fbdbf8a
madymadesc <madymadesc@gmail.com>
2022-04-14 13:43:36 +0000

    first commit

---
+++ js/image.js
@@ -0,0 +1,47 @@
+// Image 1
+function img1() {
+    document.getElementById("img1").innerHTML = '<img src="images/portfolio/portfolio-1.jpg" style="width:150%; height:300px ;" onclick="img1Default()"/>';
+}
+function img1Default() {
+    document.getElementById("img1").innerHTML = '<img src="images/portfolio/portfolio-1.jpg" style="width:100%;" onclick=" img1()"/>';
+}
+
+// Image 2
+function img2() {
+    document.getElementById("img2").innerHTML = '<img src="img7.jpg" style="width:500px;" onclick="default1()"/>';
+}
+function img2Default() {
+    document.getElementById("img2").innerHTML = '<img src="img7.jpg" style="width:250px;" onclick="myFunction1()"/>';
+}
+
+// Image 3
+function img3() {
+    document.getElementById("img3").innerHTML = '<img src="img7.jpg" style="width:500px;" onclick="default1()"/>';
+}
+function img3Default() {
+    document.getElementById("img3").innerHTML = '<img src="img7.jpg" style="width:250px;" onclick="myFunction1()"/>';
+}
+
+// Image 4
+function img4() {
+    document.getElementById("img4").innerHTML = '<img src="img7.jpg" style="width:500px;" onclick="default1()"/>';
+}
+function img4Default() {
+    document.getElementById("img4").innerHTML = '<img src="img7.jpg" style="width:250px;" onclick="myFunction1()"/>';
+}
+
+// Image 5
+function img5() {
+    document.getElementById("img5").innerHTML = '<img src="img7.jpg" style="width:500px;" onclick="default1()"/>';
+}
+function img5Default() {
+    document.getElementById("img5").innerHTML = '<img src="img7.jpg" style="width:250px;" onclick="myFunction1()"/>';
+}
+
+// Image 6
+function img6() {
+    document.getElementById("img6").innerHTML = '<img src="img7.jpg" style="width:500px;" onclick="default1()"/>';
+}
+function img6Default() {
+    document.getElementById("img6").innerHTML = '<img src="img7.jpg" style="width:250px;" onclick="myFunction1()"/>';
+}
---
+++ js/script.js
@@ -0,0 +1,21 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:["","Développeur Web et Mobile","Web Designer","UX / UI Designer",""],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+var typedd = new Typed(".typi",{
+    strings:["","Développeur Web et Mobile","Web Designer","UX / UI Designer",""],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+var typeddd = new Typed(".side",{
+    strings:["","Développeur Web et Mobile","Web Designer","Développeur Web et Mobile","Web Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
